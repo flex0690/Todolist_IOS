2d3426f6d2ae57af339381cd58f21ffb35d2d28c
tarun.kumar <tarun.kumar1@telekom-digital.com>
2025-05-14 16:09:47 +0530

    Initial Commit

---
+++ Todolist/TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistApp.swift
+//  Todolist
+//
+//  Created by Tarun Kumar on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
